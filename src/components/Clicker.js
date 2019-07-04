fd25284252c938da0b0bf67b8fdcf6371c03551c
evrenvural <evrenvural4@gmail.com>
2019-07-04 15:30:49 +0300

    The project is ready for publish

--- src/components/Clicker.js
+++ src/components/Clicker.js
@@ -4,15 +4,38 @@ import "../Clicker.css";
 export default class Clicker extends Component {
     constructor(props) {
         super(props);
+
+        this.state = { result: 0 }
     }
+
+    increase = () => {
+        const newValue = this.state.result + 1;
+        this.setState({
+            result: newValue
+        });
+    }
+
+    decrease = () => {
+        const newValue = this.state.result - 1;
+        this.setState({
+            result: newValue
+        });
+    }
+
+    reset = () => {
+        this.setState({
+            result: 0
+        });
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="clicker">
-                    <div id="result">0</div>
-                    <button id="left"><i className="fa fa-plus fa-2x"></i></button>
-                    <button id="mid"><i className="fa fa-refresh fa-2x"></i></button>
-                    <button id="right"><i className="fa fa-minus fa-2x"></i></button>
+                    <div id="result">{this.state.result}</div>
+                    <button id="left" onClick={this.increase} > <i className="fa fa-plus fa-2x"></i></button>
+                    <button id="mid" onClick={this.reset} > <i className="fa fa-refresh fa-2x"></i></button>
+                    <button id="right" onClick={this.decrease}> <i className="fa fa-minus fa-2x"></i></button>
                 </div>
             </div>
 
